9ef2024be237a4dc1342b1582dcda2708700c4cc
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-12-04 18:53:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here
+class Polygon
+{
+  constructor(sides)
+  {
+    this._sides = sides;
+  }
+
+  get countSides()
+  {
+    return this._sides.length();
+  }
+
+  get perimeter()
+  {
+    const reducer = (accumulator, currentValue) => accumulator + currentValue;
+    return this._sides.reduce(reducer);
+  }
+}
